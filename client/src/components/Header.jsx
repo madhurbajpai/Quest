c7433eb5e89492b37bcf04163d2cab8d2762c4f2
madhurbajpai <mdhrbajpai@gmail.com>
2023-11-05 14:10:28 +0530

    initialize home page

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Abhijeet Code Here</div>
+  )
+}
+
+export default Header
---
+++ client/src/components/HomeMid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeMid = () => {
+  return (
+    <div>Shantanu Code Here</div>
+  )
+}
+
+export default HomeMid
