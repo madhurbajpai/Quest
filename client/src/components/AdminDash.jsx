0874972577b6f449089d8c87a6b1eac588dee18b
madhurbajpai <mdhrbajpai@gmail.com>
2023-11-06 14:18:02 +0530

    modifying admin

--- client/src/components/AdminDash.jsx
+++ client/src/components/AdminDash.jsx
@@ -3,48 +3,109 @@ import Header from "./Header";
 import "./AdminDash.css";
 const AdminDash = () => {
   return (
-    <div className="admin-main">
-      <Header />
-      <div className="admin-btn-grp">
-        <button type="button" class="btn btn-primary">
-          Create New Quizz
-        </button>
-        <button type="button" class="btn btn-primary">
-          Create Random Quizz
-        </button>
-      </div>
-      <div className="admin-history">
-        <table class="table table-striped table-hover" id='table-history'>
-          <thead>
-            <tr>
-              <th scope="col">S. No.</th>
-              <th scope="col">Title</th>
-              <th scope="col"></th>
-              <th scope="col">Handle</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <html>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+      </head>
+      <body>
+        <div className="admin-main">
+          <Header />
+          <div className="admin-info">
+            <h1 className="user-head">Welcome, Admin</h1>
+            <span class="material-symbols-outlined" id='admin-account'>account_circle</span>
+          </div>
+          <hr />
+          <div className="admin-btn">
+            <h1 className="history-head">Custom/Random Quizes</h1>
+          <div className="admin-btn-grp">
+            <button type="button" className="btn btn-primary">
+              Create New Quizz
+            </button>
+            <button type="button" className="btn btn-primary">
+              Create Random Quizz
+            </button>
+          </div>
+          </div>
+          <hr />
+          <div className="admin-history">
+            <h1 className="history-head">Previous Quizes</h1>
+            <table
+              className="table table-striped table-hover"
+              id="table-history"
+            >
+              <thead>
+                <tr>
+                  <th scope="col">S. No.</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Duration</th>
+                  <th scope="col">Attempted</th>
+                  <th scope="col">Manage</th>
+                  <th scope="col">Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Geography</td>
+                  <td>01/11/2023</td>
+                  <td>15 mins</td>
+                  <td>40</td>
+                  <td>
+                    <span class="material-symbols-outlined">visibility</span>
+                  </td>
+                  <td>
+                    <span class="material-symbols-outlined">delete</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>History</td>
+                  <td>02/11/2023</td>
+                  <td>20 mins</td>
+                  <td>35</td>
+                  <td>
+                    <span class="material-symbols-outlined">visibility</span>
+                  </td>
+                  <td>
+                    <span class="material-symbols-outlined">delete</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Polity</td>
+                  <td>03/11/2023</td>
+                  <td>30 mins</td>
+                  <td>20</td>
+                  <td>
+                    <span class="material-symbols-outlined">visibility</span>
+                  </td>
+                  <td>
+                    <span class="material-symbols-outlined">delete</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>General Knowledge</td>
+                  <td>04/11/2023</td>
+                  <td>15 mins</td>
+                  <td>10</td>
+                  <td>
+                    <span class="material-symbols-outlined">visibility</span>
+                  </td>
+                  <td>
+                    <span class="material-symbols-outlined">delete</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 };
 
