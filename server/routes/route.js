792f80491146c418273e1e42f58bea79608cd082
madhurbajpai <mdhrbajpai@gmail.com>
2023-11-14 16:14:45 +0530

    quiz-controller updated

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { addUser} from '../controller/userController.js';
 import { addAdmin } from '../controller/adminController.js';
-import { addQuiz } from '../controller/quizController.js';
 import { getUser } from '../controller/userController.js';
 import { getAdmin } from '../controller/adminController.js';
+import { addQuiz } from '../controller/quizController.js';
+import { getQuiz } from '../controller/quizController.js';
 const router = express.Router();
 
 router.get('/', (req,res) => {
@@ -14,5 +15,7 @@ router.post('/register-user',addUser);
 router.post('/register-admin',addAdmin);
 router.post('/login-user', getUser);
 router.post('/login-admin', getAdmin);
+router.post('/add-quiz', addQuiz)
+router.get('/get-quiz/:quizId', getQuiz);
 
 export default router;
