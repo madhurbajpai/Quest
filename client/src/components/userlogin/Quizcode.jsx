0bea2595f658bb7554fb72fa9e2273c42fde6262
Shantanu Aswal <shantanuaswal9@gmail.com>
2023-11-17 02:18:34 +0530

    user login code added

---
+++ client/src/components/userlogin/Quizcode.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './quizcode.css'
+
+const Quizcode = () => {
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">Verification Code</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+      <div className="input">
+       <input type="text" />
+      </div>
+      <div className="input">
+       <input type="text" />
+      </div>
+      <div className="input">
+       <input type="text" />
+      </div>    
+      </div>
+    </div>
+
+    
+      
+  )
+}
+
+export default Quizcode;
